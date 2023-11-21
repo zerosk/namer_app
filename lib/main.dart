4ef42641d7278a9bc01da723a60dc145a2116308
Hector Sosa <zero.sk8@gmail.com>
2023-11-20 19:43:18 -0600

    Two secctions

--- lib/main.dart
+++ lib/main.dart
@@ -45,62 +45,89 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
     IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
-    } else{
+    } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-            BigCard(pair: pair),
-            SizedBox(height: 20),
-        
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-
               ElevatedButton.icon(
                 onPressed: () {
                   appState.toggleFavorite();
-                  print('se arego a fovoritos');
                 },
                 icon: Icon(icon),
                 label: Text('Like'),
-                
-              ), 
-
-              SizedBox(width: 20),
-
+              ),
+              SizedBox(width: 10),
               ElevatedButton(
-                  onPressed: () {
-                    print('button pressed!');
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
             ],
           ),
-        
-        
-          ],
-        ),
+        ],
       ),
     );
   }
 }
+
+
 
 class BigCard extends StatelessWidget {
   const BigCard({
